e2c1fcc8362e5dce5ee0977905be4d4e05ec8941
melikaayoughi <melikaayoughi@gmail.com>
2019-05-21 15:59:06 +0200

    gan train, still bugs are left

--- assignment_3/code/a3_gan_template.py
+++ assignment_3/code/a3_gan_template.py
@@ -6,24 +6,27 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torchvision import datasets
+import matplotlib.pyplot as plt
 
 
 class Generator(nn.Module):
     def __init__(self, latent_dim, input_dim=784, h_dim=128):
         super(Generator, self).__init__()
-        # in the generator we go from latent space  z to input space x
 
+        self.latent_dim = latent_dim
+
+        # in the generator we go from latent space  z to input space x
         self.generator = nn.Sequential(
             nn.Linear(latent_dim, h_dim),  # latent_dim -> 128
             nn.LeakyReLU(0.2),
             nn.Linear(h_dim, 2 * h_dim),  # 128 -> 256
-            nn.BatchNorm1d(),
+            nn.BatchNorm1d(2 * h_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(2 * h_dim, 4 * h_dim),  # 256 -> 512
-            nn.BatchNorm1d(),
+            nn.BatchNorm1d(4 * h_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(4 * h_dim, 8 * h_dim),  # 512 -> 1024
-            nn.BatchNorm1d(),
+            nn.BatchNorm1d(8 * h_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(8 * h_dim, input_dim),  # 1024 -> input_dim
             nn.Tanh()
@@ -40,9 +43,9 @@ class Discriminator(nn.Module):
         self.descriminator = nn.Sequential(
             nn.Linear(input_dim, h_dim),  # input_dim -> 512
             nn.LeakyReLU(0.2),
-            nn.Linear(h_dim, h_dim / 2),  # 512 -> 256
+            nn.Linear(h_dim, int(h_dim / 2)),  # 512 -> 256
             nn.LeakyReLU(0.2),
-            nn.Linear(h_dim / 2, 1),  # 256 -> 1
+            nn.Linear(int(h_dim / 2), 1),  # 256 -> 1
             nn.Sigmoid()
         )
 
@@ -51,43 +54,77 @@ class Discriminator(nn.Module):
 
 
 def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
-    for epoch in range(args.n_epochs):
-        for i, (imgs, _) in enumerate(dataloader):
 
-            imgs.cuda()
+    gen_curve, disc_curve = [], []
+    for epoch in range(args.n_epochs):
+        losses_d, losses_g = [], []
+        for i, (imgs, _) in enumerate(dataloader):
+            print(imgs.shape)
+            # imgs.cuda() # what is this?
+            imgs = imgs.reshape(imgs.shape[0], -1) # [batch_size, 784] make images 1-dimensional
 
             # Train Generator
             # ---------------
-            with optimizer_D.no_grad:
+
+            D_X = discriminator.forward(imgs)
+
+            # Sample
+            z = torch.randn((imgs.shape[0], generator.latent_dim))
+            generated_img = generator.forward(z)
+            D_GZ = discriminator.forward(generated_img)
+
+            loss_g = -torch.mean(torch.log(D_GZ))
+            loss_d = -(torch.mean(torch.log(D_X)) + torch.mean(torch.log(1-D_GZ)))
+
+            optimizer_G.zero_grad()
+            optimizer_D.zero_grad()
+            loss_d.backward()
+            loss_g.backward()
+            optimizer_G.step()
+            optimizer_D.step()
+
+            losses_d.append(loss_d.item())
+            losses_g.append(loss_g.item())
 
             # Train Discriminator
             # -------------------
-            optimizer_D.zero_grad()
 
             # Save Images
             # -----------
+
             batches_done = epoch * len(dataloader) + i
             if batches_done % args.save_interval == 0:
-                # You can use the function save_image(Tensor (shape Bx1x28x28),
-                # filename, number of rows, normalize) to save the generated
-                # images, e.g.:
-                # save_image(gen_imgs[:25],
-                #            'images/{}.png'.format(batches_done),
-                #            nrow=5, normalize=True)
-                pass
+                # Sample
+                z = torch.randn((args.n_samples, generator.latent_dim))
+                generated_img = generator.forward(z)
+                generated_img = generated_img.reshape(-1, 1, 28, 28)
+                save_image(generated_img, f"grid_Epoch{epoch}.png", nrow=4, padding=2, normalize=True)
 
+        gen_curve.append(losses_g)
+        disc_curve.append(losses_d)
+    save_elbo_plot(gen_curve, disc_curve, 'images/loss.pdf')
+
+def save_elbo_plot(generator_curve, discriminator_curve, filename):
+    plt.figure(figsize=(12, 6))
+    plt.plot(generator_curve, label='generator')
+    plt.plot(discriminator_curve, label='discriminator')
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.tight_layout()
+    plt.savefig(filename)
 
 def main():
     # Create output image directory
     os.makedirs('images', exist_ok=True)
+    device = torch.device(args.device)
 
     # load data
     dataloader = torch.utils.data.DataLoader(
         datasets.MNIST('./data/mnist', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5),
-                                                (0.5, 0.5, 0.5))])),
+                           transforms.Normalize((0.5,), (0.5,))])),
         batch_size=args.batch_size, shuffle=True)
 
     # Initialize models and optimizers
@@ -99,9 +136,7 @@ def main():
     # Start training
     train(dataloader, discriminator, generator, optimizer_G, optimizer_D)
 
-    # You can save your generator here to re-use it to generate images for your
-    # report, e.g.:
-    # torch.save(generator.state_dict(), "mnist_generator.pt")
+    torch.save(generator.state_dict(), args.save_model)
 
 
 if __name__ == "__main__":
@@ -118,6 +153,10 @@ if __name__ == "__main__":
                         help='save every SAVE_INTERVAL iterations')
     parser.add_argument('--device', type=str, default="cuda:0",
                         help="Training device 'cpu' or 'cuda:0'")
+    parser.add_argument('--save_model', type=str, default="./gan_generator.pt",
+                        help="Path to a file to save the model on")
+    parser.add_argument('--n_samples', default=16, type=int,
+                        help='number of samples')
     args = parser.parse_args()
 
     main()
